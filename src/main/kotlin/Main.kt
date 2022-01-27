9050bce9410e9c942c72d4361cecb2a5873023cb
Jaewang <wodhkd8@naver.com>
2022-01-27 17:46:14 +0900

    단계2 상품카테고리메뉴 출력

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,4 +11,31 @@ fun main(args: Array<String>) {
         Input category you want.
         ***=====================***
         """.trimIndent())
+
+    /*
+    *
+     */
+
+    val categories = arrayOf("fashion", "tech", "pets")
+    for (category in categories) {
+        println(category)
+    }
+    
+    println("==> 장바구니로 이동하시려면 '#'을 입력하세요")
+    //
+    var selectedCategory = readLine()
+    
+    // 사용자 입력을 반복처리
+    // 사용자가  올바른 입력할 때까지 반복처리
+    while (selectedCategory.isNullOrBlank()) {
+        println("값을 입력하세요")
+        selectedCategory = readLine()
+    }
+    
+    if (selectedCategory == "#") {
+        // TODO 1 자바구니로 
+    } else {
+        // TODO 2 카테고리 상품 목록 보여주기
+        // TODO 3 카테고리 목록에 업슨ㄴ 경우 처리
+    } 
 }
